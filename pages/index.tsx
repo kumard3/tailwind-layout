484e6716e923508ae5fab3f4f3f7c88dc2c2f6ab
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-24 16:16:23 +0530

    document file added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,11 @@
 /* eslint-disable @next/next/link-passhref */
-import Link from 'next/link';
-import * as React from 'react';
+import Link from "next/link";
+import * as React from "react";
 
 export default function Home() {
   return (
-      <main className="flex justify-center items-center w-full h-full">
-        <section className='bg-white flex justify-center items-center w-full'>
-          <div className=' flex flex-col text-3xl justify-center font-bold items-center min-h-screen text-center'>
-            <h1 className='text-4xl'>Next.js + Tailwind CSS + TypeScript Starter</h1>
-            <p className='mt-2 text-xl text-gray-800'>
-              A starter for Next.js, Tailwind CSS, and TypeScript with Seo.
-            </p>
-            <p className='mt-[1rem] text-lg '>
-            </p>
-          </div>
-        </section>
-      </main>
+    <div>
+      <h1>Hello {`<World></World>`}</h1>
+    </div>
   );
 }
