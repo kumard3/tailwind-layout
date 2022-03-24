3d4fc907acc5c6ec479e15d39d401bc03255ccbf
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-24 19:49:04 +0530

    header added

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="container mx-auto py-10 w-full text-center font-mono">
+      <h1 className="text-5xl font-bold"> Tailwind-CSS Layout</h1>
+      <h1 className="text-lg py-4 font-semibold">
+        Popular layouts and patterns made with Tailwind-CSS
+      </h1>
+      <h1>Following covers are made with Tailwind-CSS only.</h1>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,28 @@
 /* eslint-disable @next/next/link-passhref */
 import Link from "next/link";
 import * as React from "react";
+import Header from "../components/Header";
+
+const Nav = () => {
+  return (
+    <div className=" text-5xl font-mono font-semibold text-center py-10">
+      <h1>TailWind-CSS Layout</h1>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello {`<World></World>`}</h1>
+    <div className="min-h-screen">
+      {/* <Nav /> */}
+      <Header />
+      <div className="relative container mx-auto flex flex-col justify-center items-center w-full">
+        <div className="sticky top-0 text-5xl font-mono px-2 w-full sm:px-10 font-semibold flex items-center">
+          <div className="bg-gray-200 h-[1px] w-1/2"></div>
+          <h1 className="px-3 text-gray-100">Display</h1>{" "}
+          <div className="bg-gray-200 h-[1px] w-1/2"></div>
+        </div>
+      </div>
     </div>
   );
 }
